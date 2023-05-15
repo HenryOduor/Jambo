52e660fdbc84592ceb73e0fbe726af1230d0f98c
HenryOduor <115753876+HenryOduor@users.noreply.github.com>
2023-05-15 12:18:35 +0000

    Message History and input

--- jambo-web/src/pages/index.js
+++ jambo-web/src/pages/index.js
@@ -12,20 +12,37 @@ export default function Home() {
   },]);
   
   const sendRequest =async ()=>{
-    const response=await fetch(API_URL,{
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json",
-        Authorization: `Bearer ${apiKey}`,
+    const updatedMessages=[
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
       },
-      body:JSON.stringify({
-        model:"gpt-3.5-turbo",
-        messages:[{ role: "user", content: "Hello!" }],
-      }),
-    });
-    const resJson=await response.json();
-    console.log(resJson);
-    setBotMessage(resJson.choices[0].message.content)
+    ];
+    setMessages(updatedMessages);
+    setUserMessage("");
+    try {
+      const response=await fetch(API_URL,{
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body:JSON.stringify({
+          model:"gpt-3.5-turbo",
+          messages:updatedMessages,
+        }),
+      });
+      const resJson=await response.json();
+      console.log(resJson);
+      const updatedMessages2 = [...updatedMessages, resJson.choices[0].message];
+      setMessages(updatedMessages2);
+    }
+      catch (error) {
+        console.error("error");
+        window.alert("Error:" + error.message);
+    }
+    
   };
   
   return (
@@ -44,9 +61,34 @@ export default function Home() {
           </div>
         </div>
       </nav>
-
-
+      <div className="flex-1 overflow-y-scroll">
+          <div className="mx-auto w-full max-w-screen-md p-4 ">
+            {messages.filter((msg) => msg.role !== "system").map((msg, idx) => (
+                <div key={idx} className="mt-3">
+                  <div className="font-bold">
+                    {msg.role === "user" ? "You" : "Jobot"}
+                  </div>
+                  <div className="text-lg">{msg.content}</div>
+                </div>
+              ))}
+          </div>
+      </div>
+      <div className="mx-auto w-full max-w-screen-md px-4 pt-0 pb-2 flex">
+          <textarea
+            className="border rounded-md text-lg p-2 flex-1"
+            rows={1}
+            placeholder="Ask me anything..."
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+          />
+          <button
+            onClick={sendRequest}
+            className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white px-4 ml-2"
+          >
+            Send
+          </button>
+        </div>
     </div>
-  
-  )
+          
+  );
 }
